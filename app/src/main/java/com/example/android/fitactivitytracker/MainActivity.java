c380c5928bae95c7f2fef324ac671fe1b02669c4
SawS 智奇 <SawS@macbookpro-b6f9.colgate.edu>
2016-06-02 16:45:34 -0400

    Finished support for Distance

--- app/src/main/java/com/example/android/fitactivitytracker/MainActivity.java
+++ app/src/main/java/com/example/android/fitactivitytracker/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.android.fitactivitytracker;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,6 +50,7 @@ import static java.text.DateFormat.getTimeInstance;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "FitActivityTracker";
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
     public static final String SAMPLE_SESSION_NAME = "Today Workout";
 
     private static final String AUTH_PENDING = "auth_state_pending";
@@ -61,6 +71,22 @@ public class MainActivity extends AppCompatActivity {
             authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
         }
 
+        if (!checkPermissions()) {
+            requestPermissions();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // This ensures that if the user denies the permissions then uses Settings to re-enable
+        // them, the app will start working.
+        executeTask();
+    }
+
+
+    private void executeTask() {
         ((EditText)findViewById(R.id.val_check_goal_edit_text)).setOnEditorActionListener(
                 new TextView.OnEditorActionListener() {
                     @Override
@@ -72,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                                 event.getAction() == KeyEvent.ACTION_DOWN &&
                                         event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
                             //if (!event.isShiftPressed()) {
-                                // the user is done typing.
+                            // the user is done typing.
 
                             //}
                             buildFitnessClient();
@@ -120,55 +146,57 @@ public class MainActivity extends AppCompatActivity {
         String numStepsStr = numStepsET.getText().toString();
         EditText calExpended = (EditText) findViewById(R.id.calories_expended_edit_text);
         String caloriesStr = calExpended.getText().toString();
-        //EditText distanceET = (EditText) findViewById(R.id.distance_traveled_edit_text);
-        //String distanceStr = distanceET.toString();
-        //String[] retStr = {numStepsStr, caloriesStr, distanceStr};
-        String[] retStr = {numStepsStr, caloriesStr};
+        EditText distanceET = (EditText) findViewById(R.id.distance_traveled_edit_text);
+        String distanceStr = distanceET.getText().toString();
+        String[] retStr = {numStepsStr, caloriesStr, distanceStr};
+
         return retStr;
     }
 
     private void buildFitnessClient() {
         // Create the Google API Client
-        mClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.HISTORY_API)
-                .addApi(Fitness.SESSIONS_API)
-                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ_WRITE))
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
-                .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
-                .addConnectionCallbacks(
-                        new ConnectionCallbacks() {
-                            @Override
-                            public void onConnected(Bundle bundle) {
-                                Log.i(TAG, "Connected!!!");
-                                // Now you can make calls to the Fitness APIs.  What to do?
-                                // Look at some data!!
-                                EditText valCheckGoalET = (EditText) findViewById(R.id.val_check_goal_edit_text);
-                                String valCGS = valCheckGoalET.getText().toString();
-                                String[] goalStr = valAllEditText();
-                                goalPeriod = Integer.valueOf(valCGS);
-                                new ReadDataAndCompareTask().execute(goalStr);
-                            }
+        if (mClient == null && checkPermissions()){
+            mClient = new GoogleApiClient.Builder(this)
+                    .addApi(Fitness.HISTORY_API)
+                    .addApi(Fitness.SESSIONS_API)
+                    .addScope(new Scope(Scopes.FITNESS_LOCATION_READ_WRITE))
+                    .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                    .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
+                    .addConnectionCallbacks(
+                            new ConnectionCallbacks() {
+                                @Override
+                                public void onConnected(Bundle bundle) {
+                                    Log.i(TAG, "Connected!!!");
+                                    // Now you can make calls to the Fitness APIs.  What to do?
+                                    // Look at some data!!
+                                    EditText valCheckGoalET = (EditText) findViewById(R.id.val_check_goal_edit_text);
+                                    String valCGS = valCheckGoalET.getText().toString();
+                                    String[] goalStr = valAllEditText();
+                                    goalPeriod = Integer.valueOf(valCGS);
+                                    new ReadDataAndCompareTask().execute(goalStr);
+                                }
 
-                            @Override
-                            public void onConnectionSuspended(int i) {
-                                // If your connection to the sensor gets lost at some point,
-                                // you'll be able to determine the reason and react to it here.
-                                if (i == ConnectionCallbacks.CAUSE_NETWORK_LOST) {
-                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
-                                } else if (i == ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
-                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                @Override
+                                public void onConnectionSuspended(int i) {
+                                    // If your connection to the sensor gets lost at some point,
+                                    // you'll be able to determine the reason and react to it here.
+                                    if (i == ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                        Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                    } else if (i == ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                        Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                    }
                                 }
                             }
+                    )
+                    .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+                            Log.i(TAG, "Google Play services connection failed. Cause: " +
+                                    result.toString());
                         }
-                )
-                .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(ConnectionResult result) {
-                        Log.i(TAG, "Google Play services connection failed. Cause: " +
-                                result.toString());
-                    }
-                })
-                .build();
+                    })
+                    .build();
+        }
     }
 
     private class ReadDataAndCompareTask extends AsyncTask<String[], Void, String[]> {
@@ -195,10 +223,13 @@ public class MainActivity extends AppCompatActivity {
             //currently only supports first two, [will fix it]
             String text1;
             String text2;
+            String text3;
             int numStepsDiff = Integer.valueOf(result[0]);
             float caloriesDiff = Float.valueOf(result[1]);
+            int distanceDiff = Integer.valueOf(result[2]);
             Log.i(TAG,"numStepsDiff: "+ numStepsDiff);
             Log.i(TAG,"caloriesDiff: "+ caloriesDiff);
+            Log.i(TAG,"distanceDiff: "+ distanceDiff);
             if (numStepsDiff <= 0) {
                 text1 = "Yay, you've completed your goal for number of steps.\n";
             } else {
@@ -209,8 +240,13 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 text2 = "Your number of calories expended is " + caloriesDiff + " short of your goal.\n";
             }
+            if (distanceDiff <= 0) {
+                text3 = "Yay, you've completed your goal for distance covered.\n";
+            } else {
+                text3 = "Your distance covered is " + distanceDiff + " short of your goal.\n";
+            }
             TextView textView = (TextView) findViewById(R.id.return_msg_text_view);
-            String returnMsg = text1+text2;
+            String returnMsg = text1+text2+text3;
             textView.setText(returnMsg);
         }
     }
@@ -252,7 +288,7 @@ public class MainActivity extends AppCompatActivity {
 
                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
-                //.aggregate(DataType.TYPE_DISTANCE_DELTA, DataType.AGGREGATE_DISTANCE_DELTA)
+                .aggregate(DataType.TYPE_DISTANCE_DELTA, DataType.AGGREGATE_DISTANCE_DELTA)
 
                 .bucketByTime(goalPeriod, TimeUnit.DAYS)
                 .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
@@ -289,7 +325,7 @@ public class MainActivity extends AppCompatActivity {
         //int retVal = -1;
         //List<Integer> retVList = new ArrayList<Integer>();
         //int i = 0;
-        List<String> strL = new ArrayList<String>(2);
+        List<String> strL = new ArrayList<String>(3);
         if (dataReadResult.getBuckets().size() > 0) {
             Log.i(TAG, "Number of returned buckets of DataSets is: "
                     + dataReadResult.getBuckets().size());
@@ -308,7 +344,7 @@ public class MainActivity extends AppCompatActivity {
                 strL.add(str);
             }
         }
-        return strL.toArray(new String[2]);
+        return strL.toArray(new String[3]);
     }
 
     //tempoaray checkGoal function - only check for total accumulated values for each dataSet
@@ -332,12 +368,18 @@ public class MainActivity extends AppCompatActivity {
                 float val = Float.valueOf(""+value);
                 float goalVal = Float.valueOf(""+goal[1]);
                 return Float.toString(goalVal-val);
+            } else if (dataType.equals(DataType.TYPE_DISTANCE_DELTA)) {
+                int val = Integer.valueOf(""+value);
+                int goalVal = Integer.valueOf(""+goal[2]);
+                return Integer.toString(goalVal-val);
             }
         } else if (dpL.size() == 0){
             if (dataType.equals(DataType.TYPE_STEP_COUNT_DELTA)){
                 return goal[0];
             } else if (dataType.equals(DataType.TYPE_CALORIES_EXPENDED)) {
                 return goal[1];
+            } else if (dataType.equals(DataType.TYPE_DISTANCE_DELTA)) {
+                return goal[2];
             }
         }
         return "error";
@@ -396,6 +438,94 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         // [END delete_dataset]
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            Snackbar.make(
+                    findViewById(R.id.main_activity_view),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    })
+                    .show();
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted.
+                buildFitnessClient();
+            } else {
+                // Permission denied.
+
+                // In this Activity we've chosen to notify the user that they
+                // have rejected a core permission for the app since it makes the Activity useless.
+                // We're communicating this message in a Snackbar since this is a sample app, but
+                // core permissions would typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                Snackbar.make(
+                        findViewById(R.id.main_activity_view),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.settings, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Build intent that displays the App settings screen.
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        })
+                        .show();
+            }
+        }
     }
 
     @Override
